7042fce9fccd21dda61f6077a6a93e8160492235
JLRuba26 <joserubenlopezrubalcaba@gmail.com>
2021-11-24 07:55:25 -0700

    as

--- guardar.php
+++ guardar.php
@@ -4,9 +4,10 @@
  $apertura  = $_POST['txt_Aper'];
  $cierre  = $_POST['txt_Cierre'];
  $venta = $_POST['Venta_T'];
- $fecha = $_POST['Fecha']
+ $fecha = $_POST['Fecha'];
+ $IDnegocio = 1;
 
-$insertar = "INSERT INTO Reportes VALUES ('$apertura','$cierre','$venta','$fecha') ";
+$insertar = "INSERT INTO Reportes VALUES ('$apertura','$cierre','$venta','$fecha',$IDnegocio)";
 
 $query = mysqli_query($conectar, $insertar);
 
