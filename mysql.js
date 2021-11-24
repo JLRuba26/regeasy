064aeffd94d388595f799a9e5665a39485e3cc76
JLRuba26 <joserubenlopezrubalcaba@gmail.com>
2021-11-24 09:04:43 -0700

    sa

--- mysql.js
+++ mysql.js
@@ -12,3 +12,21 @@ con.connect( (err) =>{
     console.log("Funciona")
 })
 
+exports.handler = (valores, context, callback) => {
+    // allows for using callbacks as finish/error-handlers
+    context.callbackWaitsForEmptyEventLoop = false;
+    const sql = "INSERT INTO Reportes VALUES ('"+valores.apertura+"','"+valores.cierre+"','"+valores.venta+"','"+valores.fecha+"')";
+    con.query(sql, (err, res) => {
+      if (err) {
+        throw err
+      }
+      callback(null, 'Se registro valor.');
+    });
+      
+  };
+
+
+//con.query("INSERT INTO Reportes VALUES ('$apertura','$cierre','$venta','$fecha',$IDnegocio)"),function(error,results){
+//if (error)throw error;
+
+//});
