f6853382643aec37e3833c532de831fb67a50299
JLRuba26 <joserubenlopezrubalcaba@gmail.com>
2021-11-24 07:44:42 -0700

    Actualizacion de mas cosas

---
+++ conexion.php
@@ -0,0 +1,13 @@
+<?php
+
+$host = "https://71k85v4qzj.execute-api.us-east-2.amazonaws.com/PrimerInsertar";
+$user = "admin";
+$clave = "Tenemosun3312";
+$bd  = "regeasy";
+
+$conectar = mysqli_connect($host,$user,$clave,$bd);
+
+
+
+
+?>
---
+++ guardar.php
@@ -0,0 +1,30 @@
+<?php
+ require 'conexion.php';
+  
+ $apertura  = $_POST['txt_Aper'];
+ $cierre  = $_POST['txt_Cierre'];
+ $venta = $_POST['Venta_T'];
+ $fecha = $_POST['Fecha']
+
+$insertar = "INSERT INTO Reportes VALUES ('$apertura','$cierre','$venta','$fecha') ";
+
+$query = mysqli_query($conectar, $insertar);
+
+if($query){
+
+   echo "<script> alert('correcto');
+    location.href = '../index.html';
+   </script>";
+
+}else{
+    echo "<script> alert('incorrecto');
+    location.href = '../index.html';
+    </script>";
+}
+
+
+
+
+
+
+?>
