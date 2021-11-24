53a71862dc545d43e2e42c533c35e7cf3765c346
JLRuba26 <joserubenlopezrubalcaba@gmail.com>
2021-11-24 04:51:49 -0700

    Proceder
    Espero que no truene

---
+++ mysql.js
@@ -0,0 +1,15 @@
+const mysql = require('mysql');
+const con = mysql.createConnection({
+  host: 'database-regeasy.c9nn9omq8yfi.us-east-2.rds.amazonaws.com',
+  user: 'admin',
+  port:"3306",
+  password: 'Tenemosun3312',
+  database: 'regeasy',
+});
+
+con.connect( (err) =>{
+    if(err) throw err
+    console.log("Funciona")
+})
+
+con.end()
