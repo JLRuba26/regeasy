a875879350e1d62b14ef91674d233953e596d368
JLRuba26 <joserubenlopezrubalcaba@gmail.com>
2021-11-24 07:48:52 -0700

    as

--- guardar.php
+++ guardar.php
@@ -13,12 +13,12 @@ $query = mysqli_query($conectar, $insertar);
 if($query){
 
    echo "<script> alert('correcto');
-    location.href = '../index.html';
+    location.href = 'registros.html';
    </script>";
 
 }else{
     echo "<script> alert('incorrecto');
-    location.href = '../index.html';
+    location.href = 'registros.html';
     </script>";
 }
 
